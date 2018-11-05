de0e7c75d405505b689cb1ef0280938cdf029fd0
dahoon <crysis1@naver.com>
2018-11-05 14:07:01 +0900

    Create index.html and main.css,main.js

---
+++ VanillaJS/day1105-giphy-search-engine/main.js
@@ -0,0 +1,10 @@
+// 1. input 태그 안의 값을 잡는다
+const inputValue = document.querySelector('#search').value;
+console.log(inputValue);
+const button = document.querySelector('#search-btn');
+button.addEventListener('click',() => {
+    
+});
+// 2. giphy라는 API를 활용하여 data를 받고 가공한다.
+
+// 3. GIF 파일들을 index.html에 넣는다.
