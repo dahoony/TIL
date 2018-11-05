c97b31e9829e8646fdfd6d2d6e3fae4fb115aa0b
dahoon <crysis1@naver.com>
2018-11-05 15:36:33 +0900

    Get JSON on Giphy api

--- VanillaJS/day1105-giphy-search-engine/main.js
+++ VanillaJS/day1105-giphy-search-engine/main.js
@@ -14,14 +14,25 @@ inputArea.addEventListener('keypress',(event)=>{
 });
 
 // 2. giphy라는 API를 활용하여 data를 받고 가공한다.
-const apiKey = 'pETal56SsBpImEECWh6AcSOUlSZfg7zU';
+const API_KEY = 'pETal56SsBpImEECWh6AcSOUlSZfg7zU';
 const keyword = 'cats';
-const URL = `api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${apiKey}`;
-console.log(URL);
+const URL = `http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${API_KEY}`;
+
+// Ajax request 
+const GiphyAjaxCall = new XMLHttpRequest();
+GiphyAjaxCall.open("GET",URL);
+GiphyAjaxCall.send();
+
+GiphyAjaxCall.addEventListener('load', (event) => {
+    const rawData = event.target.response;
+    const parsedData = JSON.parse(rawData);
+    pushToDOM(parsedData);
+});
 
 // 3. GIF 파일들을 index.html에 넣는다.
-const pushToDOM = (data)=>{
+const pushToDOM = (parsedData)=>{
+    //console.log(parsedData.data[0].images.fixed_height.url);
     const result_area = document.querySelector('#result-area');
-
-    result_area.innerHTML = data;
+    
+    result_area.innerHTML = `<img src="${parsedData.data[0].images.fixed_height.url}" alt='cat'>`;
 };
