9c223edb0a0b5f447117c05d67bc9a8a7eeb7ec4
dahoon <crysis1@naver.com>
2018-11-05 14:28:57 +0900

    Add EventListener to DOM

--- VanillaJS/day1105-giphy-search-engine/main.js
+++ VanillaJS/day1105-giphy-search-engine/main.js
@@ -1,10 +1,23 @@
 // 1. input 태그 안의 값을 잡는다
-const inputValue = document.querySelector('#search').value;
-console.log(inputValue);
 const button = document.querySelector('#search-btn');
+const inputArea = document.querySelector('#search');
 button.addEventListener('click',() => {
-    
+    const inputValue = document.querySelector('#search').value;
+    pushToDOM(inputValue);
 });
+
+inputArea.addEventListener('keypress',(event)=>{
+    if(event.key === 'Enter'){
+        const inputValue = document.querySelector('#search').value;
+        pushToDOM(inputValue);
+    }
+});
+
 // 2. giphy라는 API를 활용하여 data를 받고 가공한다.
 
 // 3. GIF 파일들을 index.html에 넣는다.
+const pushToDOM = (data)=>{
+    const result_area = document.querySelector('#result-area');
+
+    result_area.innerHTML = data;
+};
