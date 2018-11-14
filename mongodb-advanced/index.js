047fdd39c78671873d8edb453e456505b49627cc
dahoony <crysis1@naver.com>
2018-11-14 13:16:49 +0900

    Create Author

---
+++ mongodb-advanced/index.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+/** Connect */
+mongoose.connect('mongodb://localhost/relation',{ useNewUrlParser: true })
+    .then(()=> console.log('Connected MongoDB'))
+    .catch(error => console.error(error.message));
+
+/** Modeling */
+
+const Author = mongoose.model('Author',new mongoose.Schema({
+    name: {type: String, required:true, minlength:2},
+    github: String    
+}));
+
+
+const Course = mongoose.model('Course',new mongoose.Schema({
+    name:{
+        type: String,
+        minlength:3,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Author'
+    }
+}));
+
+/** CREATE */
+async function createAuthor(name, github){
+    const author = new Author({
+        name,
+        github
+    });
+
+    try{
+        const result = await author.save();
+        console.log(result);
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+async function createCourse(name, author){
+    const course = new Course(name,author);
+    try{
+        const result = await course.save();
+        console.log(result);
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+createAuthor('최다훈','github.com');
+// createCourse('coding',)
